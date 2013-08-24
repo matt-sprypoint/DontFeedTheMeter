1df7ae8f17dc62474aade50ffafd8af12e3e790f
Matt Duffy <mduffy@sprypoint.com>
2013-08-24 20:20:12 -0300

    uncommented default h2 db config

--- app/models/MeterMaid.java
+++ app/models/MeterMaid.java
@@ -1,7 +1,10 @@
 package models;
 
+import javax.persistence.Entity;
+
 import play.db.ebean.Model;
 
+@Entity
 public class MeterMaid extends Model {
 	
 	/**
--- app/models/ParkingLocation.java
+++ app/models/ParkingLocation.java
@@ -2,8 +2,11 @@ package models;
 
 import java.util.Date;
 
+import javax.persistence.Entity;
+
 import play.db.ebean.Model;
 
+@Entity
 public class ParkingLocation extends Model {
 	
 	/**
